0bf64a95c9be9e9b6300fdf273f82fdf2d4ebd2f
Jay Jagpal <jay@strangemother.com>
2023-09-28 04:44:41 +0100

    Functional cleanup

--- gen_maze2.py
+++ gen_maze2.py
@@ -1,7 +1,105 @@
 import turtle
 import random
 import json
+
+import importlib.util
+import sys
+
+# For illustrative purposes.
+import tokenize
 from pathlib import Path
+
+def main():
+    run_all()
+
+
+COLORS = [
+    None,
+    'black',
+    'blue',
+    'green',
+]
+
+
+def make_screen():
+    global win
+
+    # Set up the screen
+    win = turtle.Screen()
+    win.title("Maze Generator")
+    win.bgcolor("#333")
+    return win
+
+
+def run_all():
+    global grid_size, cell_size, items
+
+    win = make_screen()
+
+    shape = json.loads(Path('./plans/four.json').read_text())
+    # shape = json.loads(Path('./plans/fly.json').read_text())
+    maze_data = shape
+    grid_size = maze_data["count"]
+    items = maze_data["items"]
+    # print(win.tracer())
+    win.tracer(0, 0)
+
+    # Create a turtle to draw the maze
+
+    maze_turtle = turtle.Turtle()
+    GAME['maze_turtle'] = maze_turtle
+    maze_turtle.shape("turtle")
+    maze_turtle.speed(0)
+    maze_turtle.penup()
+
+    # Define the cell size
+    cell_size = 50
+    start_cell = (2,-1)
+    # Draw the maze based on the JSON representation
+    for i in range(grid_size):
+        for j in range(grid_size):
+            shape_cell = items[i * grid_size + j]
+            is_wall = shape_cell >= 1
+            color = COLORS[shape_cell]
+
+            if shape_cell == 2: #start position
+                start_cell = (i, j)
+            if not is_wall:
+                continue
+
+            draw_block(i,j, grid_size, cell_size, color, maze_turtle)
+
+    win.tracer(1, 10)
+
+
+    # x,y = draw_block(*start_cell, grid_size, cell_size, 'green')
+    x,y = ij_to_xy(*start_cell, grid_size, cell_size)
+    # i,j = xy_to_ij(x,y,grid_size, cell_size)
+    # print(start_cell, x,y, i,j)
+    start_position = (x+(cell_size*.5), y-(cell_size*.5))
+
+    # draw_block(2,grid_size, grid_size, cell_size, 'blue')
+
+    win.tracer(1, 10)
+
+
+    # Start the update loop
+    print('Start', start_position)
+    maze_turtle.goto(*start_position)
+    maze_turtle.color('red')
+    maze_turtle.speed(3)
+    maze_turtle.pensize(5)
+    maze_turtle.pendown()
+    update_turtle(maze_turtle)
+
+    # Hide the drawing turtle
+    # maze_turtle.hideturtle()
+    add_routines_from('', 'motion.py', maze_turtle=maze_turtle)
+    # Run the Turtle graphics
+    try:
+        turtle.mainloop()
+    except KeyboardInterrupt:
+        sys.exit()
 
 
 
@@ -20,27 +118,6 @@ def render_cell(x,y, cell_size=10, color='black', unit=None):
     unit.penup()
 
 
-shape = json.loads(Path('./plans/three.json').read_text())
-# shape = json.loads(Path('./plans/fly.json').read_text())
-maze_data = shape
-grid_size = maze_data["count"]
-items = maze_data["items"]
-
-# Set up the screen
-win = turtle.Screen()
-win.title("Maze Generator")
-win.bgcolor("#333")
-# print(win.tracer())
-win.tracer(0, 0)
-
-# Create a turtle to draw the maze
-
-maze_turtle = turtle.Turtle()
-maze_turtle.shape("turtle")
-maze_turtle.speed(0)
-maze_turtle.penup()
-
-
 def draw_block(i,j, grid_size, cell_size, color='black', unit=None):
     x,y = ij_to_xy(i,j, grid_size, cell_size)
     render_cell(x,y,cell_size, color or 'black', unit)
@@ -55,48 +132,12 @@ def ij_to_xy(i,j, grid_size, cell_size=10):
     y = (grid_size * cell_size) // 2 - i * cell_size
     return x, y
 
+
 def xy_to_ij(x, y, grid_size, cell_size=10):
     """Convert an onscreen x/y coord to a grid location (i,j of grid_size)"""
     i = (grid_size - 1) - (y + (grid_size * cell_size) // 2) // cell_size
     j = (x + (grid_size * cell_size) // 2) // cell_size
     return i, j
-
-COLORS = [
-    None,
-    'black',
-    'blue',
-    'green',
-]
-
-# Define the cell size
-cell_size = 50
-start_cell = (2,-1)
-# Draw the maze based on the JSON representation
-for i in range(grid_size):
-    for j in range(grid_size):
-        shape_cell = items[i * grid_size + j]
-        is_wall = shape_cell >= 1
-        color = COLORS[shape_cell]
-
-        if shape_cell == 2: #start position
-            start_cell = (i, j)
-        if not is_wall:
-            continue
-
-        draw_block(i,j, grid_size, cell_size, color)
-
-win.tracer(1, 10)
-
-
-# x,y = draw_block(*start_cell, grid_size, cell_size, 'green')
-x,y = ij_to_xy(*start_cell, grid_size, cell_size)
-# i,j = xy_to_ij(x,y,grid_size, cell_size)
-# print(start_cell, x,y, i,j)
-start_position = (x+(cell_size*.5), y-(cell_size*.5))
-
-# draw_block(2,grid_size, grid_size, cell_size, 'blue')
-
-win.tracer(1, 10)
 
 
 def steps(count=1):
@@ -110,11 +151,13 @@ GAME = {
     'success': None,
     'error': None,
     'path': (),
+    'maze_turtle': None
 }
 
-def update():
+def update_turtle(maze_turtle=None):
     # Move the player forward
     # Get the player's position in the grid
+    maze_turtle = maze_turtle or GAME['maze_turtle']
     pos = maze_turtle.pos()
     x = int(pos[0])
     y = int(pos[1])
@@ -126,7 +169,7 @@ def update():
         # no position here
         shape_cell = None
 
-    print('update', maze_turtle.pos(), i,j, shape_cell)
+    print('update', maze_turtle.pos(), f"{i=},{j=}, {shape_cell=}")
     # if shape_cell > 0:
         # import pdb; pdb.set_trace()  # breakpoint 3cd26586 //
 
@@ -140,10 +183,11 @@ def update():
         GAME['error'] = f'Fail at cell, {shape_cell}'
 
     # Call the update function again after a delay (e.g., 100 milliseconds)
-    if GAME['match'] < 4:
-        turtle.ontimer(update, 10)
-    else:
-        check()
+    if GAME['match'] < 5:
+        turtle.ontimer(update_turtle, 10)
+        return
+
+    check()
 
 
 def check():
@@ -170,8 +214,13 @@ def check():
         # draw_block(i,j, grid_size, cell_size, color, path_turtle)
         x,y = ij_to_xy(i,j, grid_size, cell_size)
         path_turtle.goto(x+(cell_size*.5),y-(cell_size*.5))
-        print('shape', f"{i=},{j=}, {shape_cell=}")
-        path_turtle.pencolor('red')
+
+        if shape_cell in [0, 2, 3]:
+            # Good!
+            path_turtle.pencolor('green')
+        else:
+            path_turtle.pencolor('red')
+        # print('shape', f"{i=},{j=}, {shape_cell=}")
         path_turtle.stamp()
 
     print(set(types))
@@ -186,21 +235,25 @@ def check():
 def end(*a):
     sys.exit()
 
-# Start the update loop
-print('Start', start_position)
-maze_turtle.goto(*start_position)
-maze_turtle.color('red')
-maze_turtle.speed(3)
-maze_turtle.pensize(5)
-maze_turtle.pendown()
-update()
-maze_turtle.forward(steps(grid_size+1))
-# Hide the drawing turtle
-# maze_turtle.hideturtle()
-import sys
 
-# Run the Turtle graphics
-try:
-    turtle.mainloop()
-except KeyboardInterrupt:
-    sys.exit()
+def add_routines_from(module_name=None, file_path=None, **kw):
+    module_name = module_name or tokenize.__name__
+    file_path = file_path or tokenize.__file__
+    # if module_name not in sys.modules:
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+
+
+    provides = ['steps', 'grid_size']
+    g = globals()
+    for key in provides:
+        setattr(module, key, g[key])
+    for k, v in kw.items():
+        setattr(module, k, v)
+    spec.loader.exec_module(module)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ motion.py
@@ -0,0 +1,3 @@
+
+print(__file__, maze_turtle)
+maze_turtle.forward(steps(grid_size))
